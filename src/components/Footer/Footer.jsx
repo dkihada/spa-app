dbb61bf9236944db63804f83aa5892d84a839c1d
Denis Agureev <dkihada@gmail.com>
2021-09-22 14:24:00 +0500

    create Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div class='ui inverted vertical footer segment form-page'>
+      <div class='ui container'>SPA React Project Portfolio</div>
+    </div>
+  );
+}
+
+export { Footer };
