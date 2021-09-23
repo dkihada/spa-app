3c7ee3f996591e9531d3b2f1d18bf4214ffb7f5d
Denis Agureev <dkihada@gmail.com>
2021-09-23 16:51:34 +0500

    create Preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,14 @@
+function Preloader() {
+  return (
+    <div class='ui segment'>
+      <div class='ui active dimmer'>
+        <div class='ui massive text loader'>Loading</div>
+      </div>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
+
+export { Preloader };
